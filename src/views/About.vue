44b389b9c799729a6dd65a2dcef4fd45cc49c4fe
ismail <ismail@gmail.com>
2022-07-20 21:26:40 +0200

    update

--- src/views/About.vue
+++ src/views/About.vue
@@ -14,4 +14,5 @@ export default {
   <pf-modal v-model:open="open1" title="Simple modal header">
     <img src="http://localhost:8780/svg/processes/incidents/instances/3f29ac88-205c-4739-b759-ca26b2fcb52c"/>
   </pf-modal>
+  <h1></h1>
 </template>
